54fcb3666f616f00b11d47a41f2f12578f217b1c
Ruchita <ruchita.mourya@gmail.com>
2018-04-20 23:39:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ruchi/tictactoe/ViewModel/TicTacToeVm.java
@@ -0,0 +1,25 @@
+package com.example.ruchi.tictactoe.ViewModel;
+
+import android.util.Log;
+import android.view.View;
+
+public class TicTacToeVm {
+
+    private String data;
+
+    public TicTacToeVm() {
+        this.data = "O";
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public void onClickTab(View view){
+        Log.d("zxcv", "Clicked ");
+    }
+}
